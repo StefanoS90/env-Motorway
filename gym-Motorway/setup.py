29b6a880620d14720692764bea2a72c50c27debb
stefano <stefano.sabatini90@gmail.com>
2019-11-01 18:42:00 +0100

    minor changes

--- gym-Motorway/setup.py
+++ gym-Motorway/setup.py
@@ -4,6 +4,5 @@ from setuptools import setup
 
 setup(name='gym_Motorway',
       version='0.0.1',
-	  packages=setuptools.find_packages(),
       install_requires=['gym','baseconvert']  # And any other dependencies foo needs
 )
